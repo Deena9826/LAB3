a6d2fdaabc4eadea46965afcb137d742473f3b1f
Deena9826 <46736347+Deena9826@users.noreply.github.com>
2019-02-14 10:34:34 +0700

    Add files via upload

---
+++ CSW/Deena.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.min.css') }}"/>
+<h1>
+<title>Deena</title>
+<h1>
+</head>
+<body>
+
+<h1>This is a Heading</h1>
+<p>This is a paragraph.</p>
+
+<img src="https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj6oo7hqafgAhWKWX0KHQ8PBSkQjRx6BAgBEAU&url=https%3A%2F%2Fgifer.com%2Fen%2FIbC8&psig=AOvVaw0kXNWsE3RJpPJhuNWizTiD&ust=1549549990965079"> 
+
+</body>
+</html>
